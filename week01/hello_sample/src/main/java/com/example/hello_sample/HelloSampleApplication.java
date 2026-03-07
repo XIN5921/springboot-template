a01619737ca8a78e4242f2cd0399bd25ebf43441
XIN5921 <3389747021@qq.com>
2026-03-07 10:03:11 +0800

    增加hello接口及相关截图

---
+++ week01/hello_sample/src/main/java/com/example/hello_sample/HelloSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSampleApplication.class, args);
+    }
+
+}
---
+++ week01/hello_sample/src/test/java/com/example/hello_sample/HelloSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
